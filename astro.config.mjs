122c675b3367ac496cc7e9bcc566d6d245f4b275
Abner <1228428816@qq.com>
2024-05-23 19:16:27 +0800

    chore: Update site URL in astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,6 @@ import { defineConfig } from 'astro/config';
 
 import preact from "@astrojs/preact";
 
-// https://astro.build/config
 export default defineConfig({
   site: "https://cdt3211.top",
   integrations: [preact()],
