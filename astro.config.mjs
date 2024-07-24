828de8f23da25f9e99263ebb211ab5ed50247348
Abner <1228428816@qq.com>
2024-07-24 23:29:58 +0800

    feat: Update about page with dynamic history events

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,18 @@
 import { defineConfig } from 'astro/config';
 import preact from "@astrojs/preact";
 import vue from "@astrojs/vue";
+import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://cdt3211.top",
-  integrations: [preact(), vue()]
+  integrations: [preact(), vue(), mdx()],
+  markdown: {
+    shikiConfig: {
+      themes: {
+        light: 'github-light',
+        dark: 'github-dark',
+      },
+    },
+  },  
 });
