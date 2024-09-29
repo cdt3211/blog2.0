439afc699b1f2faeab881e79d4c48ce13e98666c
Abner <1228428816@qq.com>
2024-09-29 19:24:35 +0800

    Update

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,17 +2,18 @@ import { defineConfig } from 'astro/config';
 import preact from "@astrojs/preact";
 import vue from "@astrojs/vue";
 import mdx from "@astrojs/mdx";
+import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://cdt3211.top",
-  integrations: [preact(), vue(), mdx()],
+  integrations: [preact(), vue(), mdx(), react()],
   markdown: {
     shikiConfig: {
       themes: {
-        light: 'github-light',
-        dark: 'github-dark',
-      },
-    },
-  },  
+        light: 'aurora-x',
+        dark: 'github-dark'
+      }
+    }
+  }
 });
